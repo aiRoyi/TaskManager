4f09181728f5a92b467dd0837f84d97cafa56f60
cmy <a4527943@qq.com>
2019-03-09 20:23:11 +0800

    add post test

--- TaskManager.Test/FunctionalTest/TaskMangerTest.cs
+++ TaskManager.Test/FunctionalTest/TaskMangerTest.cs
@@ -2,9 +2,12 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Newtonsoft.Json;
+using System.Net;
 using System.Net.Http;
+using System.Text;
 using TaskManager.MVC.Web;
 using TaskManager.MVC.Web.Models;
+using TaskManager.MVC.Web.ViewModels;
 using TaskManager.Test.Utils;
 using Xunit;
 
@@ -21,8 +24,10 @@ namespace TaskManager.Test.FunctionalTest
                 .UseStartup<Startup>())
                 .CreateClient();
             var userModel = new UserModel { Id = 1, UserName = "chenmeiyi", Password = "cmy123456" };
-            var result = client.GetStreamAsync("Account/Login").Result;
-            var error = client.GetStreamAsync("Account/Login").Exception;
+            var jsonString = JsonConvert.SerializeObject(userModel);
+            var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            var result = client.PostAsJsonAsync("Account/Login", content).Result;
+            var error = client.PostAsJsonAsync("Account/Login", content).Exception;
             Assert.NotNull(result);
             Assert.Null(error);
         }
@@ -40,6 +45,19 @@ namespace TaskManager.Test.FunctionalTest
             var error = client.GetStreamAsync("Account/Register").Exception;
             Assert.NotNull(result);
             Assert.Null(error);
+        }
+
+        [Fact]
+        public void CreateTaskTest()
+        {
+            var client = new TestServer(WebHost
+                .CreateDefaultBuilder()
+                .UseContentRoot(ProjectUtil.GetProjectPath("TaskManager.sln", "", typeof(Startup).Assembly))
+                .UseStartup<Startup>())
+                .CreateClient();
+            var model = new UpdateTaskViewModel { Description = "test001", Status = TaskStatus.NotDone, Tag = "test", Type = "test" };
+            var result = client.PostAsJsonAsync("Task/Create", new { taskModel = model, userId = 1, userName = "chenmeiyi" }).Result;
+            Assert.NotNull(result);
         }
     }
 }
